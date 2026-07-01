0274800f67165c7223ad83cb475f41d69a9ecbac
Sourav Dewett <souravdewett@MacBookPro.lan>
2026-07-01 14:38:35 -0700

    for-loop

---
+++ Loops/for.py
@@ -0,0 +1,13 @@
+"""
+Print 1 to 20 as in 1..3..5..7..9...
+"""
+
+# for i in range (1, 22, 2) :
+#     print(i, end = " ");
+
+"""
+Print 10 to 1 using for loop
+"""
+
+for i in range (10, 0 , -1) :
+    print(i, end = " ");
