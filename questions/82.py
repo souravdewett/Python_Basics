c555a9debb6baa0f355c32acab99bcb184cfb902
Sourav Dewett <souravdewett@MacBookPro.lan>
2026-07-02 16:59:34 -0700

    intermediate-pattern-questions

---
+++ questions/82.py
@@ -0,0 +1,13 @@
+"""
+Print the following pattern :
+5 4 3 2 1
+5 4 3 2 
+5 4 3 
+5 4
+5
+"""
+for i in range(1, 6) :
+    for j in range (5, i - 1, -1) : # pehli vaar ch kinne column chahide
+        print(j, end = " ");
+    print();
+    
